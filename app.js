8bca48e6c69dfc10a9a28341444659d9a7af729e
Renzo Monteleone <47033935+monteleonerenzo@users.noreply.github.com>
2020-08-20 22:05:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -18,4 +18,7 @@ app.use(express.static('assets'));
  
 app.use('/assets', express.static(__dirname + '/assets'));
  
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
