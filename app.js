30b77d675e1807758b24ed9c4d736312881cf2e5
Renzo Monteleone <renzomonteleone@gmail.com>
2020-08-21 00:03:31 -0300

    0.1

--- app.js
+++ app.js
@@ -22,3 +22,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
+
+
+app.post("/pagar", function (req, res) {
+    alert("PAGA")
+  });
